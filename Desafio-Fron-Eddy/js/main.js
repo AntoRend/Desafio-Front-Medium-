e39101c7d4e1747100d1e82b00cfa03ad3a59aff
CodeStart-ER <eddyrual@gmail.com>
2020-03-30 20:21:49 -0500

    2020/03/20 updating files adding some improvements in navbar functionality

--- Desafio-Fron-Eddy/js/main.js
+++ Desafio-Fron-Eddy/js/main.js
@@ -1,6 +1,6 @@
 
 // duration of scroll animation
-var scrollDuration = 300;
+var scrollDuration = 500;
 // paddles
 var leftPaddle = document.getElementsByClassName('left-paddle');
 var rightPaddle = document.getElementsByClassName('right-paddle');
@@ -28,7 +28,7 @@ var getMenuSize = function() {
 };
 var menuSize = getMenuSize();
 // get how much of menu is invisible
-var menuInvisibleSize = menuSize - menuWrapperSize;
+var menuInvisibleSize = menuSize ; //"- menuWrapperSize"
 
 // get how much have we scrolled to the left
 var getMenuPosition = function() {
@@ -39,31 +39,12 @@ var getMenuPosition = function() {
 $('.menu').on('scroll', function() {
 
 	// get how much of menu is invisible
-	menuInvisibleSize = menuSize - menuWrapperSize;
+	menuInvisibleSize = menuSize; //"- menuWrapperSize"
 	// get how much have we scrolled so far
 	var menuPosition = getMenuPosition();
 
 	var menuEndOffset = menuInvisibleSize - paddleMargin;
 
-	// show & hide the paddles 
-	// depending on scroll position
-	if (menuPosition <= paddleMargin) {
-		$(leftPaddle).addClass('hidden');
-		$(rightPaddle).removeClass('hidden');
-	} else if (menuPosition < menuEndOffset) {
-		// show both paddles in the middle
-		$(leftPaddle).removeClass('hidden');
-		$(rightPaddle).removeClass('hidden');
-	} else if (menuPosition >= menuEndOffset) {
-		$(leftPaddle).removeClass('hidden');
-		$(rightPaddle).addClass('hidden');
-}
-
-	// print important values
-	$('#print-wrapper-size span').text(menuWrapperSize);
-	$('#print-menu-size span').text(menuSize);
-	$('#print-menu-invisible-size span').text(menuInvisibleSize);
-	$('#print-menu-position span').text(menuPosition);
 
 });
 
